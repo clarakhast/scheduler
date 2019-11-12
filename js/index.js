083425520d0ad6fcffbb0738b1fd2392e31d7913
Clara Khast <n01337555@nj-00118-007.humber.org>
2019-11-12 12:02:52 -0500

    array exercise solved

--- js/index.js
+++ js/index.js
@@ -1,53 +1,57 @@
-const courseName = `Tools and Workflow`;
-const courseCode = `WDDM-115`;
-const courseInstructor = `Kadeem Best`;
-const courseStart = `Fall 2019`;
-const courseWeeks = 15; // 1 class per week (assumed)
-const courseBreaks = true; // Boolean
-const courseDuration = 160; // Minutes per class
+const allCourses = [
+  {
+    name: `Tools and Workflow`,
+    code: `WDDM-115`,
+    instructor: `Kadeem Best`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: `Yes`,
+    duration: 160
+  }
+  ,
+  {
+    name: `Planning and Prototyping`,
+    code: `WDDM-117`,
+    instructor: `Cory Coletta`,
+    start: { term: `Fall`, year: 2019 },
+    weeks: 15,
+    breaks: `Yes`,
+    duration: 160
+  }
+];
 
 
-const course = {
-  name: `Tools and Workflow`,
-  code: `WDDM-115`,
-  instructor: `Kadeem Best`,
-  start: { term: `Fall`, year: 2019 },
-  weeks: 15,
-  breaks: true,
-  duration: 160
-};
+// function setCourseHtml(course) {
+//   document.getElementById(`name`).innerHTML = course.name;
+//   document.getElementById(`code`).innerHTML = course.code;
+//   document.getElementById(`instructor`).innerHTML = course.instructor;
+//   document.getElementById(`start`).innerHTML = `${course.start.term} ${course.start. year}`;
+//   document.getElementById(`weeks`).innerHTML = course.weeks;
+//   document.getElementById(`breaks`).innerHTML = course.breaks;
+//   document.getElementById(`duration`).innerHTML = getDurationFromMinutes(course.duration);
+// }
 
-document.getElementById(`name`).innerHTML = course.name;
-document.getElementById(`code`).innerHTML = course.code;
-document.getElementById(`instructor`).innerHTML = course.instructor;
-document.getElementById(`start`).innerHTML = `${course.start.term}` + ` ` + `${course.start. year}`;
-document.getElementById(`weeks`).innerHTML = course.weeks;
-// document.getElementById(`breaks`).innerHTML = course.breaks;
-document.getElementById(`duration`).innerHTML = getDurationFromMinutes(course.duration);
+// setCourseHtml(allCourses[0]);
 
-/*
-LAB:
-1. Fix the output of the start term by turning the Object's properties into a String
-      Ex: { term: `Fall`, year: 2019 }  >>  `Fall 2019`
-2. Write a function that will take a Number of minutes and will return a human-readable String
-      Ex: 160  >>  `2 hr, 40 min`
-      Hints:
-        - Division: /
-        - Remainder: %
-        - Rounding:
-            - Standard rules:     Math.round(4.5)
-            - Always round up:    Math.ceil(1.001)
-            - Always round down:  Math.floor(1.999)
-*/
 
-if (course.breaks === true) {
-  document.getElementById(`breaks`).innerHTML = `Yes`;
+function getCourseAsHtmlString(course){
+  document.getElementById(`courses`).innerHTML = 
+  `<h3 id="name">${course.name}</h3>
+  <ul>
+    <li>Course Code: <strong id="code">${course.code}</strong></li>
+    <li>Instructor: <strong id="instructor">${course.instructor}</strong></li>
+    <li>Start: <strong id="start">${course.start.term} ${course.start. year}</strong></li>
+    <li>
+      Weeks: <strong id="weeks">${course.weeks}</strong>
+      <ul>
+        <li>Includes Break: <strong id="breaks">${course.breaks}</strong></li>
+      </ul>
+    </li>
+    <li>Duration: <strong id="duration">${getDurationFromMinutes(course.duration)}</strong></li>
+  </ul>`;
 }
 
-else {
-  document.getElementById(`breaks`).innerHTML = `No`;
-}
-
+getCourseAsHtmlString(allCourses[0]);
 
 function getDurationFromMinutes(minutes) {
   var hours = Math.floor(minutes / 60); 
